c5d2a30671a42fd3f8294f5da4c611847819465f
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-12-19 21:52:39 +0000

    Import Router from shop.js --> app.js

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(adminRoutes);
+app.use(shopRoutes);
 
-
-// app.use('/', (req, res, next) => {
-//     res.send('<h1>Hello from Express!</h1>');
-// });
 
 app.listen(3000);

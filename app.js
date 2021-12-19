5c9c630c1ee8554bd7c484323d4c122ffed2208b
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2021-12-19 21:19:04 +0000

    Add /add-product route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express')
 const app = express();
 
 
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="user_input"><button type="submit">Add Product</button></form>');
+});
+
 app.use('/', (req, res, next) => {
     res.send('<h1>Hello from Express!</h1>');
 });

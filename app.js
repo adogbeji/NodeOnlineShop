85ed8da478e8c7cc7bdb2722d6697a168cb9cf7a
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2022-01-01 15:59:07 +0000

    Change name of unhandled routes page

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {  // 'catch-all' route for dealing with unhandled routes
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 

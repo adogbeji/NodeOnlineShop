0b28aa1813f02d35d0d8eb8d2c27e6ba9dd94000
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2022-04-02 12:32:01 +0100

    Add 404 page controller

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express')
 const bodyParser = require('body-parser');
 
+// 404 page controller
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -19,9 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));  // Serves files static
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {  // 'catch-all' route for dealing with unhandled routes
-    res.status(404).render('404', { pageTitle: 'Page Not Found!'});
-});
-
+app.use(errorController.get404);
 
 app.listen(3000);
---
+++ controllers/error.js
@@ -0,0 +1,3 @@
+exports.get404 = (req, res, next) => {  // 'catch-all' route for dealing with unhandled routes
+    res.status(404).render('404', { pageTitle: 'Page Not Found!'});
+};

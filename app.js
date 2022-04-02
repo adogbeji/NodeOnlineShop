2692f646b4c9c5f6f871e04bf42c074aa49936f6
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2022-04-02 12:15:53 +0100

    Add controllers

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));  // Serves files statically
 
 // Outsourced routes
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {  // 'catch-all' route for dealing with unhandled routes

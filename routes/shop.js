c4a78021e0f5ec4b8da798d1d30007413b9e2b28
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2022-09-01 16:51:49 +0100

    Add controller for orders page & register in shop.ejs

--- routes/shop.js
+++ routes/shop.js
@@ -14,7 +14,7 @@ router.get('/products', shopController.getProducts);
 
 router.get('/cart', shopController.getCart);
 
-router.get('/orders');
+router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
 

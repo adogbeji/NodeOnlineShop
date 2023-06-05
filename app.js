2de139082f87a377ff22bb4d285ecfa0c298aa7e
adogbeji <baka_1993@yahoo.co.uk>
2023-06-05 16:40:53 +0100

    Start fetching related products

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById(1)
+  .then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -28,9 +37,21 @@ Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true })
+  // .sync({ force: true })
+  .sync()
   .then(result => {
+    return User.findById(1);
     // console.log(result);
+    // app.listen(3000);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Jack', email: 'jack@gmail.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch(err => {
